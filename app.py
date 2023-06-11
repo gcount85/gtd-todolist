7c5bf70421e9bb1a9cd65313802710d7ff51c19a
gcount85 <100mgml@gmail.com>
2023-06-11 23:01:03 +0900

    chore : Init venv
    -

--- app.py
+++ app.py
@@ -3,7 +3,7 @@
 from flask import Flask, render_template, jsonify, request
 import requests
 from bs4 import BeautifulSoup
-from pymongo import MongoClient # pymongo를 임포트 하기(패키지 인스톨 먼저 해야겠죠?)
+from pymongo import MongoClient 
 
 app = Flask(__name__)
 client = MongoClient('localhost', 27017) # mongoDB는 27017 포트로 돌아갑니다.
